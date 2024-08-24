bd91e723c2ff038c532e9c9c6347d89ead0726ec
lesley <lesleybanadzem2017@gmail.com>
2024-08-24 15:07:12 -0500

    Added functionality for adding an emoji to text

--- client/src/components/layout/chat-room/chat-room-message-input.tsx
+++ client/src/components/layout/chat-room/chat-room-message-input.tsx
@@ -1,17 +1,33 @@
 import { Button } from "../../ui/button";
 import { PaperclipIcon, SendIcon, SmileIcon } from "lucide-react";
+import EmojiPicker, { Theme } from "emoji-picker-react";
+
 import { Input } from "../../ui/input";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { type MessageInput } from "../../../types";
 import { useSendMessage } from "../../../hooks/useSendMessage";
+import { Popover, PopoverContent, PopoverTrigger } from "../../ui/popover";
+import { cn } from "../../../lib/utils";
 
 export function MessageInput({ chatId, chatName }: { chatId: string; chatName: string }) {
 	const { sendMessage } = useSendMessage();
 	const INPUT_NAME = "message_text";
-	const { register, handleSubmit, setValue } = useForm<MessageInput>(); // Update the type of useForm
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		getValues,
+		setError,
+		formState: { errors },
+	} = useForm<MessageInput>();
 
 	const onSubmit: SubmitHandler<MessageInput> = (data) => {
-		// Update the type of SubmitHandler
+		if (data.message_text.trim().length === 0) {
+			setError("message_text", {
+				message: "Can't submit an empty field",
+			});
+			return;
+		}
 		sendMessage({
 			chatId,
 			message_text: data.message_text,
@@ -19,6 +35,8 @@ export function MessageInput({ chatId, chatName }: { chatId: string; chatName: s
 		});
 		setValue(INPUT_NAME, "");
 	};
+
+	console.log(errors);
 	return (
 		<div className="p-4 border-t">
 			<form onSubmit={handleSubmit(onSubmit)} className="flex items-center">
@@ -28,14 +46,34 @@ export function MessageInput({ chatId, chatName }: { chatId: string; chatName: s
 				</Button>
 				<Input
 					type="text"
-					className="flex-1 rounded-[0.5rem]"
+					className={cn(
+						"flex-1 rounded-[0.5rem]",
+						errors?.message_text ? "border-red-400" : ""
+					)}
 					placeholder="Type a message..."
 					{...register(INPUT_NAME)}
 				/>
-				<Button type="button" variant="ghost" size="icon" className="ml-2">
-					<SmileIcon className="h-5 w-5" />
-					<span className="sr-only">Add emoji</span>
-				</Button>
+				<Popover>
+					<PopoverTrigger>
+						<Button type="button" variant="ghost" size="icon" className="ml-2">
+							<SmileIcon className="h-5 w-5" />
+							<span className="sr-only">Add emoji</span>
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent className="border-0 p-0 mr-5">
+						<EmojiPicker
+							width={300}
+							theme={Theme.DARK}
+							searchPlaceholder="Search Emoji..."
+							onEmojiClick={({ emoji }) => {
+								const currentMessageValue = getValues(INPUT_NAME);
+								const messageWithEmojiAttached = `${currentMessageValue}${emoji}`;
+
+								setValue(INPUT_NAME, messageWithEmojiAttached);
+							}}
+						/>
+					</PopoverContent>
+				</Popover>
 				<Button type="submit" size="icon" className="ml-2">
 					<SendIcon className="h-5 w-5" />
 					<span className="sr-only">Send message</span>
