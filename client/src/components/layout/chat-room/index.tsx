dedae24f6f6b2afe4e797adcd19e2428a5905fa6
lesley <lesleybanadzem2017@gmail.com>
2024-09-24 20:39:42 -0500

    Refactor chat room section to use separate components for public and private messages

--- client/src/components/layout/chat-room/index.tsx
+++ client/src/components/layout/chat-room/index.tsx
@@ -1,30 +1,35 @@
-import { useQuery } from "@tanstack/react-query";
-import api from "./../../../api/httpMethods";
-import { useParams } from "react-router";
 import { Loader } from "../../loader";
 import { MessageInput } from "./chat-room-message-input";
 import { ChatRoomSection } from "./chat-room-section";
+import { PrivateMessageSection } from "./private-chat-section";
+import useRoomData from "../../../hooks/useRoomData";
 
 function ChatRoomLayout() {
-	const { chatId } = useParams();
-
-	const { isPending, data, isFetching } = useQuery({
-		queryKey: [chatId], // Makes another call when chatId changes
-		queryFn: chatId ? () => api.fetchChatListsDataFromChatId(chatId) : undefined,
-	});
-
-	if (isFetching ?? isPending) {
+	const { loadingState, chatData, recipientData, chatId, recipientId } = useRoomData();
+	if (loadingState) {
 		return <Loader />;
 	}
 
-	if (data.error) {
+	if (chatData?.error) {
 		return <p>Error</p>;
 	}
-	const roomName = data?.msg?.[0]?.chats?.chat_name ?? "";
 
+	if (recipientData?.msg.length && recipientId) {
+		const data = recipientData.msg ?? [];
+		const recipientsName =
+			data.find(({ chat_user }) => String(chat_user?.pk_user_id) === "4")?.chat_user?.name ??
+			"";
+		return (
+			<section className="overflow-y-hidden grid grid-rows-[12fr_1fr] md:grid-rows-[11fr_1fr] ">
+				<PrivateMessageSection data={data ?? []} />
+				<MessageInput chatId={recipientId} chatName={recipientsName} isPrivateChat />
+			</section>
+		);
+	}
+	const roomName = chatData?.msg?.[0]?.chats?.chat_name ?? "";
 	return (
 		<section className="overflow-y-hidden grid grid-rows-[12fr_1fr] md:grid-rows-[11fr_1fr] ">
-			<ChatRoomSection data={data?.msg ?? []} />
+			<ChatRoomSection data={chatData?.msg ?? []} />
 			{chatId && chatId.length > 0 ? (
 				<MessageInput chatId={chatId} chatName={roomName} />
 			) : null}
---
+++ client/src/hooks/useRoomData.tsx
@@ -0,0 +1,38 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import api from "./../api/httpMethods";
+
+const useRoomData = () => {
+	const { chatId, recipientId } = useParams<{ chatId?: string; recipientId?: string }>();
+
+	const {
+		isPending: isChatPending,
+		data: chatData,
+		isFetching: isChatFetching,
+	} = useQuery({
+		queryKey: [chatId],
+		queryFn: chatId ? () => api.fetchChatListsDataFromChatId(chatId) : undefined,
+	});
+
+	const { isPending: isRecipientPending, data: recipientData } = useQuery({
+		queryKey: [recipientId],
+		queryFn: recipientId
+			? () => api.fetchPrivateMessageListsDataFromRecipientId(recipientId)
+			: undefined,
+	});
+
+	const loadingState = [
+		chatId && (isChatFetching || isChatPending),
+		recipientId && isRecipientPending,
+	].some(Boolean);
+
+	return {
+		loadingState,
+		chatData,
+		recipientData,
+		chatId,
+		recipientId,
+	};
+};
+
+export default useRoomData;
