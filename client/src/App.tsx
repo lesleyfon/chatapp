dba0861687d54444786c4051cfdd97439d7e23fb
lesley <lesleybanadzem2017@gmail.com>
2024-12-13 22:04:54 -0600

    Wrapped app in sidebar provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppRouter } from "./pages/AppRouter";
+import { SidebarProvider } from "./components/ui/sidebar";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -9,7 +10,9 @@ const queryClient = new QueryClient();
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={AppRouter} />
+			<SidebarProvider>
+				<RouterProvider router={AppRouter} />
+			</SidebarProvider>
 		</QueryClientProvider>
 	);
 }
