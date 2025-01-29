28e044b24605ae32ac1dac27a7c0593fa11a9ffa
lesley <lesleybanadzem2017@gmail.com>
2025-01-29 14:12:27 -0600

    feat: add file upload support in chat message input
    - Integrated file upload button with custom SVG icon
    - Added file input for image uploads in chat messages
    - Configured file type acceptance for PNG and JPEG
    - Updated message sending logic to include image file handling

--- client/src/components/layout/chat-room/chat-room-message-input.tsx
+++ client/src/components/layout/chat-room/chat-room-message-input.tsx
@@ -1,5 +1,5 @@
 import { Button } from "../../ui/button";
-import { PaperclipIcon, SendIcon, SmileIcon } from "lucide-react";
+import { SendIcon, SmileIcon } from "lucide-react";
 import EmojiPicker, { Theme } from "emoji-picker-react";
 
 import { Input } from "../../ui/input";
@@ -15,11 +15,15 @@ interface ChatInputProps {
 	chatName: string;
 	isPrivateChat?: boolean;
 }
+// BUTON Background Image
+const svgUrl =
+	"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IndoaXRlIiBzdHJva2Utd2lkdGg9IjEiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtcGFwZXJjbGlwIj48cGF0aCBkPSJNMTMuMjM0IDIwLjI1MiAyMSAxMi4zIi8+PHBhdGggZD0ibTE2IDYtOC40MTQgOC41ODZhMiAyIDAgMCAwIDAgMi44MjggMiAyIDAgMCAwIDIuODI4IDBsOC40MTQtOC41ODZhNCA0IDAgMCAwIDAtNS42NTYgNCA0IDAgMCAwLTUuNjU2IDBsLTguNDE1IDguNTg1YTYgNiAwIDEgMCA4LjQ4NiA4LjQ4NiIvPjwvc3ZnPg==";
 
 export function MessageInput({ chatId, chatName, isPrivateChat }: ChatInputProps) {
 	const socket = useSocket();
 	const { sendMessage, sendPrivateMessage } = useSendMessage({ socket });
 	const INPUT_NAME = "message_text";
+	const FILE_INPUT_NAME = "message_img";
 	const {
 		register,
 		handleSubmit,
@@ -36,12 +40,15 @@ export function MessageInput({ chatId, chatName, isPrivateChat }: ChatInputProps
 			});
 			return;
 		}
+		const message_img = data.message_img?.[0] as unknown as HTMLImageElement;
 
 		if (isPrivateChat) {
 			sendPrivateMessage(
 				{
 					message_text: data.message_text,
 					recipientId: chatId,
+					imageFile: message_img,
+					imageName: message_img.name,
 				},
 				socket
 			);
@@ -61,14 +68,31 @@ export function MessageInput({ chatId, chatName, isPrivateChat }: ChatInputProps
 	return (
 		<div className="p-4 border-t">
 			<form onSubmit={handleSubmit(onSubmit)} className="flex items-center">
-				<Button type="button" variant="ghost" size="icon" className="mr-2">
-					<PaperclipIcon className="h-5 w-5" />
-					<span className="sr-only">Attach file</span>
+				<Button
+					type="button"
+					variant="ghost"
+					size="icon"
+					className="mr-2 cursor-pointer"
+					style={{
+						backgroundImage: `url(${svgUrl})`,
+						backgroundRepeat: "no-repeat",
+						backgroundPositionX: "0",
+						backgroundSize: "24px",
+						backgroundPosition: "center",
+					}}
+				>
+					<Input
+						{...register(FILE_INPUT_NAME)}
+						id="message-img"
+						type="file"
+						accept="image/png, image/jpeg"
+						className=" cursor-pointer opacity-0"
+					/>
 				</Button>
 				<Input
 					{...register(INPUT_NAME)}
 					type="text"
-					className={cn("flex-1", errors?.message_text ? "border-red-400" : "")}
+					className={cn("flex-1 ", errors?.message_text ? "border-red-400" : "")}
 					placeholder="Type a message..."
 					autoComplete="off"
 				/>
