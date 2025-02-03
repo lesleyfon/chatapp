8a13b2a6be305a32da730d19ea43bae1fd217524
lesley <lesleybanadzem2017@gmail.com>
2025-02-03 14:41:18 -0600

    feat: add development environment support in environment variable retrieval

--- server/src/utils/getEnvs.ts
+++ server/src/utils/getEnvs.ts
@@ -23,6 +23,19 @@ export function getEnvs() {
     DATABASE_PASSWORD,
     DB_URL,
   }: ENV_VARS = JSON.parse(process.env.APP_ENV as string);
+
+  if(process.env.ENVIRONMENT === "development"){
+    return {
+      MONGO_CONNECTION_URL,
+      PORT,
+      JWT_SECRET,
+      JWT_LIFETIME,
+      DATABASE_HOST,
+      DATABASE_USERNAME,
+      DATABASE_PASSWORD,
+      DB_URL,
+    };
+  }
   return {
     MONGO_CONNECTION_URL,
     PORT,
@@ -31,6 +44,6 @@ export function getEnvs() {
     DATABASE_HOST,
     DATABASE_USERNAME,
     DATABASE_PASSWORD,
-    DB_URL,
   };
 }
+
