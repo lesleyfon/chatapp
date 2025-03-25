01f1552a06ed3d2834cd49e770ded311045acdd6
lesley <lesleybanadzem2017@gmail.com>
2025-03-24 19:11:33 -0500

    Added logic to get cors options in prod

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,18 +12,26 @@ dotenv.config();
 const origin: string[] = [];
 
 if(process.env.ENVIRONMENT === "development"){
-   
+
   console.log("Running app in dev mode");
   const tempOrigin:string[] = JSON.parse(process.env.APP_ENV as string).CORS_ORIGIN;
   origin.push(...tempOrigin);
 }
+
+if(process.env.ENVIRONMENT === "production"){
+  console.log("Running app in production mode");
+
+  const tempOrigin:string[] = JSON.parse(process.env.APP_ENV as string).CORS_ORIGIN;
+  origin.push(...tempOrigin);
+}
+
 const CorsOptions = {
   origin,
 };
 // CONSOLE LOG PORT TO SEE WHAT VERCEL IS SETTING AS PORT.
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3010;
 const url = process.env.ENVIRONMENT === "development" ? "http://localhost:3010": "" ;
- 
+
 console.log("URL to listen too: ", url);
 
 class SocketServer {
