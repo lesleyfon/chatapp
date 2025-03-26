a99dfbf0a595d0dc1c9f46ee1f23673ac51ca6b0
lesley <lesleybanadzem2017@gmail.com>
2025-03-26 15:11:26 -0500

    Update configuration files for TypeScript support and enhance ESLint rules. Added TypeScript parser and plugins, updated package dependencies, and removed obsolete Prettier configuration file.

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -3,21 +3,50 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      "no-console": ["error", { allow: ["warn", "error"] }],
-      "no-var": "error",
-      semi: "error",
-      indent: ["error", 2, { SwitchCase: 1 }],
-      "no-multi-spaces": "error",
-      "space-in-parens": "error",
-      "no-multiple-empty-lines": "error",
-      "prefer-const": "error",
-      "function-paren-newline": ["error", "never"],
-    },
-  },
+	{ files: ["**/*.{js,mjs,cjs,ts}"] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		files: ["**/*.{ts}"],
+		plugins: ["@typescript-eslint", "import"],
+		parser: "@typescript-eslint/parser",
+		settings: {
+			"import/internal-regex": "^~/",
+			"import/resolver": {
+				node: {
+					extensions: [".ts"],
+				},
+				typescript: {
+					alwaysTryTypes: true,
+				},
+			},
+		},
+		extends: [
+			"plugin:@typescript-eslint/recommended",
+			"plugin:@typescript-eslint/stylistic",
+			"plugin:import/recommended",
+			"plugin:import/typescript",
+			"prettier",
+		],
+		rules: {
+			"no-console": ["error", { allow: ["warn", "error"] }],
+			"no-var": "error",
+			semi: "error",
+			indent: ["error", 2, { SwitchCase: 1 }],
+			"no-multi-spaces": "error",
+			"space-in-parens": "error",
+			"no-multiple-empty-lines": "error",
+			"prefer-const": "error",
+			"function-paren-newline": ["error", "never"],
+			"import/order": [
+				"error",
+				{
+					alphabetize: { caseInsensitive: true, order: "asc" },
+					groups: ["builtin", "external", "internal", "parent", "sibling"],
+					"newlines-between": "always",
+				},
+			],
+		},
+	},
 ];
