2c1356c0a35b93194283922d4f718d54ada54f18
lesley <lesleybanadzem2017@gmail.com>
2025-04-01 10:38:44 -0500

    - Implement chat list update logic in useGetPrivateMessageList to handle message updates and new chat additions. - Introduced updateChatList function for improved state management and streamlined chat handling.

--- client/src/hooks/useGetPrivateMessageList.tsx
+++ client/src/hooks/useGetPrivateMessageList.tsx
@@ -27,6 +27,43 @@ export function isPrivateChatBetweenTwoUsers({
 	);
 }
 
+function updateChatList({
+	state,
+	response,
+	userId,
+	recipientId,
+}: {
+	state: PrivateChatResultType;
+	response: PrivateChatResultType;
+	userId: string;
+	recipientId?: string;
+}) {
+	if (!recipientId) return state;
+
+	const { sender_id, recipient_id } = state.private_chat;
+
+	if (
+		isPrivateChatBetweenTwoUsers({
+			responseSenderId: String(sender_id),
+			responseRecipientId: String(recipient_id),
+			userId: String(userId),
+			recipientId: String(recipientId),
+		})
+	) {
+		// This Updates the most recent message sent
+		return {
+			...state, // Create a new object
+			private_messages: {
+				...state.private_messages, // Preserve existing messages
+				message_text: response.private_messages.message_text, // Update the message
+				sent_at: response.private_messages.sent_at, // Update the sent_at
+			},
+		};
+	}
+
+	return state; // Return unchanged data if not the same chat
+}
+
 export const useGetPrivateMessageList = ({ socket }: { socket: Socket | null }) => {
 	const [privateRoomList, setPrivateRoomList] = useState<PrivateChatResultType[]>([]);
 	const navigate = useNavigate();
@@ -51,6 +88,8 @@ export const useGetPrivateMessageList = ({ socket }: { socket: Socket | null }) 
 			}
 		}
 
+		if (!userId) return;
+
 		setPrivateRoomList((prevList) => {
 			// Check if this chat already exists
 			const existingChatIndex = prevList.filter((chat) => {
@@ -63,17 +102,18 @@ export const useGetPrivateMessageList = ({ socket }: { socket: Socket | null }) 
 					userId: String(userId),
 					recipientId: String(recipientId),
 				});
-				return isSameChat === false;
+				return !isSameChat;
 			});
 
 			if (existingChatIndex.length === 0) {
 				// This is a new chat, add it to the list
-				return [response];
+				return [updateChatList({ state: response, response, userId, recipientId })];
 			}
 
 			// Update existing chat
-			const updatedList = existingChatIndex.concat(response);
-			return updatedList;
+			return prevList.map((chat) =>
+				updateChatList({ state: chat, response, userId, recipientId })
+			);
 		});
 	};
 
