cf69fe335767b6bc7d478a40a21012e4fb94c9e9
lesley <lesleybanadzem2017@gmail.com>
2025-04-02 11:42:00 -0500

    Refactor Login component to utilize SharedAuthInput for input fields, removing password visibility toggle logic and streamlining form structure.

--- client/src/pages/authentication/registration/index.tsx
+++ client/src/pages/authentication/registration/index.tsx
@@ -5,14 +5,13 @@ import api from "../../../api/http-methods";
 import { Button } from "../../../components/ui/button";
 import {
 	Form,
-	FormControl,
 	FormField,
 	FormItem,
 	FormLabel,
 	FormMessage,
 	FormRootError,
 } from "../../../components/ui/form";
-import { Input } from "../../../components/ui/input";
+
 import { useNavigate } from "react-router";
 import useAuthStorage from "../../../store/useAuthStorage";
 import { RegisterFormSchemaValidation } from "../validation";
@@ -20,12 +19,12 @@ import {
 	REGISTRATION_FORM_INPUT_FIELDS,
 	REGISTRATION_DEFAULT_VALUES,
 } from "../../../components/constants";
-import { useState } from "react";
-import { Eye, EyeOff } from "lucide-react";
+
+import { SharedAuthInput } from "../shared-auth-input";
 
 export function Register() {
 	const navigate = useNavigate();
-	const [showPassword, setShowPassword] = useState(false);
+
 	const authStorageLogin = useAuthStorage((state) => state.login);
 
 	const form = useForm<z.infer<typeof RegisterFormSchemaValidation>>({
@@ -57,10 +56,6 @@ export function Register() {
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
 				{REGISTRATION_FORM_INPUT_FIELDS.map((fd) => {
-					let type = fd.type;
-					if (type === "password" && showPassword) {
-						type = "text";
-					}
 					return (
 						<FormField
 							key={fd.name}
@@ -71,25 +66,7 @@ export function Register() {
 									<FormLabel className="text-left w-full flex">
 										{fd.label}
 									</FormLabel>
-									<FormControl>
-										<div className="flex flex-row">
-											<Input
-												{...field}
-												{...fd}
-												placeholder={fd.label}
-												type={type}
-											/>
-											{fd.type === "password" && (
-												<Button
-													type="button"
-													className="bg-white text-black"
-													onClick={() => setShowPassword(!showPassword)}
-												>
-													{showPassword ? <EyeOff /> : <Eye />}
-												</Button>
-											)}
-										</div>
-									</FormControl>
+									<SharedAuthInput field={field} fd={fd} />
 									<FormMessage />
 								</FormItem>
 							)}
