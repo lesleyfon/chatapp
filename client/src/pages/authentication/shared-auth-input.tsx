0792252411e89bf526bcb56a4bfe2722a288af22
lesley <lesleybanadzem2017@gmail.com>
2025-04-02 11:50:54 -0500

    - Enhance SharedAuthInput component to include input ref for password visibility toggle. - improving user experience by focusing on the input field when toggling visibility.

--- client/src/pages/authentication/shared-auth-input.tsx
+++ client/src/pages/authentication/shared-auth-input.tsx
@@ -4,7 +4,7 @@ import { Input } from "../../components/ui/input";
 import { RegisterFormSchemaValidation, LoginFormSchemaValidation } from "./validation";
 import { Button } from "../../components/ui/button";
 import { Eye, EyeOff } from "lucide-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ControllerRenderProps } from "react-hook-form";
 type FormSchema = z.infer<typeof RegisterFormSchemaValidation | typeof LoginFormSchemaValidation>;
 type NameType =
@@ -24,20 +24,31 @@ interface SharedAuthInputProps {
 
 export function SharedAuthInput({ field, fd }: SharedAuthInputProps) {
 	const [showPassword, setShowPassword] = useState(false);
+	const inputRef = useRef<HTMLInputElement>(null);
 	let type = fd.type;
 	if (type === "password" && showPassword) {
 		type = "text";
 	}
 
+	const togglePasswordVisibility = () => {
+		setShowPassword(!showPassword);
+		if (inputRef.current) {
+			// focus on the input element when toggling password visibility
+			inputRef.current.focus();
+		}
+	};
+
 	return (
 		<FormControl>
 			<div className="flex flex-row">
-				<Input {...field} {...fd} placeholder={fd.label} type={type} />
+				<Input {...field} {...fd} ref={inputRef} placeholder={fd.label} type={type} />
 				{fd.type === "password" && (
 					<Button
 						type="button"
 						className="bg-white text-black"
-						onClick={() => setShowPassword(!showPassword)}
+						onClick={togglePasswordVisibility}
+						aria-label={showPassword ? "Hide password" : "Show password"}
+						aria-pressed={showPassword}
 					>
 						{showPassword ? <EyeOff /> : <Eye />}
 					</Button>
