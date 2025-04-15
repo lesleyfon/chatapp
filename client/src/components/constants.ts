6ae2b2a89f1c448aac90d7512adab2aa26a6055b
lesley <lesleybanadzem2017@gmail.com>
2025-04-15 17:52:40 -0500

    Add DEFAULT_SVG_URL constant for default SVG image in constants.ts

--- client/src/components/constants.ts
+++ client/src/components/constants.ts
@@ -57,3 +57,6 @@ export const INPUT_NAME = "new-chat-name";
  * @description - The error field names for the new chat room.
  */
 export const ERROR_FIELD_NAMES = [INPUT_NAME, "new-chat-name-error", "new-chat-name-internal-error"];
+
+export const DEFAULT_SVG_URL =
+	"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IndoaXRlIiBzdHJva2Utd2lkdGg9IjEiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtcGFwZXJjbGlwIj48cGF0aCBkPSJNMTMuMjM0IDIwLjI1MiAyMSAxMi4zIi8+PHBhdGggZD0ibTE2IDYtOC40MTQgOC41ODZhMiAyIDAgMCAwIDAgMi44MjggMiAyIDAgMCAwIDIuODI4IDBsOC40MTQtOC41ODZhNCA0IDAgMCAwIDAtNS42NTYgNCA0IDAgMCAwLTUuNjU2IDBsLTguNDE1IDguNTg1YTYgNiAwIDEgMCA4LjQ4NiA4LjQ4NiIvPjwvc3ZnPg==";
