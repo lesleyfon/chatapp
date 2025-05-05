5819b7d03b75814aa59bc7236bdeb614e5b45e15
lesley <lesleybanadzem2017@gmail.com>
2025-05-05 14:54:34 -0500

    Add Sentry error logging for socket connection events and enhance error handling in private message functionality

--- server/src/web/socket.ts
+++ server/src/web/socket.ts
@@ -1,6 +1,8 @@
-import { StatusCodes } from 'http-status-codes';
 import { Buffer, File } from 'node:buffer';
+import * as Sentry from '@sentry/node';
+import { StatusCodes } from 'http-status-codes';
 import type { Socket, Server as SocketIOServer } from 'socket.io';
+
 import type { ExtendedError } from 'socket.io/dist/namespace';
 import { QueryHandlers } from '../model/query-handlers.model';
 import type { AddPrivateMessageType, CbType, ChatListType, JWT_RETURN_USER } from '../types';
@@ -11,6 +13,20 @@ export class AppSocketBase extends QueryHandlers {
     super();
     this.io = socket;
     this.io.use(this.socketAuthMiddleware);
+
+    this.io.engine.on('connection', (socket) => {
+      socket.on('error', (error: Error) => {
+        Sentry.captureException(error);
+      });
+
+      socket.on('disconnect', (reason: string) => {
+        Sentry.captureException({
+          message: 'User disconnected',
+          reason,
+          userId: socket.handshake.auth?.userId,
+        });
+      });
+    });
   }
 
   socketAuthMiddleware = async (socket: Socket, next: (err?: ExtendedError) => void) => {
@@ -201,7 +217,9 @@ export class AppSocketBase extends QueryHandlers {
           const privateChatsInsertResponse = (
             await this.createPrivateChatEntry(sender, receiver)
           )[0];
+          // const response = await this.createPrivateMessageAlt({ takes about 1.22 secons
           const response = await this.createPrivateMessage({
+            // Takes about 1.6 seconds
             privateChatsInsertResponse: {
               ...privateChatsInsertResponse,
               pk_private_chat_id: privateChatsInsertResponse.pk_private_chat_id,
@@ -266,8 +284,19 @@ export class AppSocketBase extends QueryHandlers {
           this.io.emit('add-private-message-response', addPrivateMessageSocketResponse);
           // Emits an event to display the most recent message sent
           this.io.emit('get-latest-private-message-sent', addPrivateMessageSocketResponse);
-        } catch (_error) {
-          // TODO: Add SENTRY logging
+        } catch (error) {
+          const hasFiles = imageFile !== null;
+          Sentry.captureException(error, {
+            extra: {
+              senderId,
+              hasFiles,
+              timezone,
+              imageName,
+              created_at,
+              recipientId,
+              method: 'addPrivateMessage',
+            },
+          });
         }
       },
     );
@@ -279,9 +308,5 @@ export class AppSocketBase extends QueryHandlers {
       this.getAUserChatList(socket);
       this.getPrivateMessageList(socket);
     });
-  }
-
-  connectToRooms(socket: SocketIOServer) {
-    socket.to(['person-1', 'person-1']);
   }
 }
