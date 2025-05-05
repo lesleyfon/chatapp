9ee0267b877bb6545b30f3ee9378fd36e983fccf
lesley <lesleybanadzem2017@gmail.com>
2025-05-05 18:06:16 -0500

    Add Zustand store for managing private messages with optimistic updates

---
+++ client/src/store/use-private-messages-store.ts
@@ -0,0 +1,15 @@
+import create from 'zustand';
+import type { PrivateChatResultType } from '../types';
+
+export const usePrivateMessagesStore = create<{
+  allRoomMessages: PrivateChatResultType[];
+  setAllRoomMessages: (msgs: PrivateChatResultType[]) => void;
+  optimisticUpdate: (msg: PrivateChatResultType) => void;
+}>()((set) => ({
+  allRoomMessages: [],
+  setAllRoomMessages: (msgs: PrivateChatResultType[]) => set({ allRoomMessages: msgs }),
+  optimisticUpdate: (msg: PrivateChatResultType) =>
+    set((state: { allRoomMessages: PrivateChatResultType[] }) => ({
+      allRoomMessages: [...state.allRoomMessages, msg],
+    })),
+}));
