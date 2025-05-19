62d8a0eb656deb9b30133769d51922dee4f42f61
lesley <lesleybanadzem2017@gmail.com>
2025-05-18 21:53:48 -0500

    Update authentication screen layout to ensure full-width responsiveness on smaller devices.

--- client/src/pages/authentication/index.tsx
+++ client/src/pages/authentication/index.tsx
@@ -18,8 +18,8 @@ export default function Authentication() {
 
   return (
     <div className='container h-screen w-screen flex justify-center items-center'>
-      <div className='authentication-screen h-72'>
-        <Tabs defaultValue='login' className='w-[560px]'>
+      <div className='authentication-screen h-72 w-full md:w-[560px]'>
+        <Tabs defaultValue='login' className='w-full md:w-[560px]'>
           <TabsList className='grid grid-cols-2 w-full gap-8 mb-8 border border-gray-100 h-fit m-0 p-2 text-[#a1a1aa]'>
             <TabsTrigger value='login' className='transition ease-in-out'>
               Login
